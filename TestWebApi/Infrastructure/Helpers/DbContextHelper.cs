35533d49168dc2438e06cb5d82655fb2170b3039
sandman633 <sad_is_bad@outlook.com>
2022-10-26 08:20:45 +0300

    added unit testing

---
+++ TestWebApi/Infrastructure/Helpers/DbContextHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+
+namespace TestWebApi.Infrastructure.Helpers
+{
+    public class DbContextHelper
+    {
+        public WebApiContext WebApiContext { get; set; }
+        public DbContextHelper()
+        {
+            var builder = new DbContextOptionsBuilder<WebApiContext>();
+            builder.UseInMemoryDatabase("UNIT_TEST_DB");
+
+            var options = builder.Options;
+            WebApiContext = new WebApiContext(options);
+        }
+    }
+}
